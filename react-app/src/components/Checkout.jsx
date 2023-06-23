5ca1a0f1d5b4c96bdf584c7c4997bba5f49d0ffb
Thorskii <utahsmile05@hotmail.com>
2023-06-23 03:23:14 -0600

    checkout optimized

--- react-app/src/components/Checkout.jsx
+++ react-app/src/components/Checkout.jsx
@@ -16,6 +16,7 @@ const Checkout = () => {
   const navigate = useNavigate();
   const {data, loading} = useFetchUser(`/users/me?populate=*`);
 
+
   useEffect(() => {
     document.title = "Checkout - Uno Distribution";  
   }, []);
@@ -39,19 +40,20 @@ const Checkout = () => {
 
   return (
     <div className="w-full items-center justify-center flex flex-col">
-        <h1 className="text-2xl">Please confirm your information.</h1><br/>
-        <form ref={form} onSubmit={sendEmail} className="flex flex-col w-1/2 h-full">
+        <h1 className="text-2xl">Please confirm your information is correct.</h1><br/>
+        <form ref={form} onSubmit={sendEmail} className="flex flex-col sm:w-3/4 md:w-1/3 h-full">
+
             <label for="fullName" className="font-bold">Business Name:</label>
-            <input type="text" value={data?.businessName} name="fullName" disabled/><br/>
+            <input type="text" value={data?.businessName} name="fullName" readonly/><br/>
             <label for="email" className="font-bold">E-mail</label>
-            <input type="email" value={data?.email} name="email" disabled/><br/>
+            <input type="email" value={data?.email} name="email" readonly/><br/>
             <label for="ein" className="font-bold">EIN</label>
-            <input type="text" value={data?.EIN} name="ein"disabled/><br/>
+            <input type="text" value={data?.EIN} name="ein" readonly/><br/>
             <label for="taxresale" className="font-bold">Tax Resale</label>
-            <input type="text" value={data?.taxResale} name="taxresale" disabled/><br/>
+            <input type="text" value={data?.taxResale} name="taxresale" readonly/><br/>
             <label for="products" className="font-bold">Order</label>
-            <div className="whitespace-pre-wrap	">{products?.map((item) => (item.title + " " + item.flavor + " " + item.quantity +" $"+ item.price + "\n"))}</div><br/>
-            <input type="submit" value="Place Order" className="hover:underline border h-[50px] w-[100px] bg-blue-500 text-white items-right"/><br/>
+            <textarea  rows={products.length+5} value={products?.map((item) => (item.quantity + "x  " + item.title + "   " + item.flavor + "   $"+ item.price + "\n")).join("")} className="whitespace-pre-wrap" readonly/><br/>
+            <input type="submit" value="Place Order" className="hover:underline hover:cursor-pointer border h-[50px] w-[100px] bg-blue-500 text-white"/><br/>
 
         </form>
 
